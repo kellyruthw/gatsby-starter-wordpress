28703f9398aed270e278928803b9f981ab7c9257
Kelly <kellyruthw@gmail.com>
2020-02-20 21:25:41 -0800

    static about page

--- src/templates/about.js
+++ src/templates/about.js
@@ -19,7 +19,8 @@ const AboutPage = (props) => {
       <div className="container">
         <div className="copy">        
             <h1>{pageData.title}</h1>
-            <p>test</p>
+            <p>Cody Vlach is a certified Paramedic with 12 years experiences within the medical field. He began his career as a Federal Firefighter traveling the country aiding those in need. His compassion for people led his to acquire his Paramedic License through Los Angeles City Fire Department shortly after becoming a Medical Instructor and EMT for local hospitals. During this period of 8 years on the ambulance he worked part time as a Football Medic studying sports medicine and nutrition.</p>
+            <p>After his time on the ambulance, his career led him to Hollywood as a Union (Local 80) Set Medic/Scuba Rescue Diver/Lifeguard. Throughout the years, Cody has consistently worked with clients around the world, such as Netflix, Apple, HBO, and Target. His desire to strive for continuing his medical capabilities has led him to become an Event Medical Coordinator and Safety Supervisor. Cody holds a degree in Fire Science from College of the Canyons and multiple other degrees and licenses from UCLA, Bakersfield and El Camino college. Throughout his professional career he has trained as an amateur bodybuilder, Scuba cave diver, Nutritionist, and SAG Actor.</p>
             </div> 
         </div>
     </Layout>
